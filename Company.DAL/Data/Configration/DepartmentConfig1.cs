8075edc599556ab965ebef3be2d6272c9c462c84
habiba <habiba15@gmail.com>
2025-03-21 02:38:10 +0200

    implementDb

---
+++ Company.DAL/Data/Configration/DepartmentConfig1.cs
@@ -0,0 +1,19 @@
+﻿using Company.DAL.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Data.Configration
+{
+    class DepartmentConfig1 : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.id).UseIdentityColumn(10, 10);
+        }
+    }
+}
