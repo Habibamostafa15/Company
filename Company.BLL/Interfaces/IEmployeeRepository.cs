8ccc51903fc28b746772634a812105a1fdeeab39
habiba <habiba.mostafa.567@gmail.com>
2025-04-01 23:07:48 +0200

    implementation of BLL for Employee Service before generic

---
+++ Company.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using Company.DAL.Dtos;
+using Company.DAL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.BLL.Interfaces
+{
+  public interface IEmployeeRepository
+    {
+
+        IEnumerable<Employee> getAll();
+        Employee? Get(int id);   // make it nullable 
+        int add(Employee model);
+        int Update(Employee model);
+
+        int delete(Employee model);
+
+
+    }
+}
---
+++ Company.BLL/Reposotiry/EmployeeRepository.cs
@@ -0,0 +1,54 @@
+﻿using Company.BLL.Interfaces;
+using Company.DAL.Data.Context;
+using Company.DAL.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.BLL.Reposotiry
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly DBContext _dBContext;
+
+        public EmployeeRepository(DBContext dBContext)
+        {
+            this._dBContext = dBContext;
+        }
+
+
+        public IEnumerable<Employee> getAll()
+        {
+            return _dBContext.Employees.ToList();
+        }
+
+        public Employee? Get(int id)
+        {
+            return _dBContext.Employees.Find(id);
+        }
+
+
+        public int add(Employee model)
+        {
+            _dBContext.Employees.Add(model);
+            return _dBContext.SaveChanges();
+        }
+
+        public int Update(Employee model)
+        {
+            _dBContext.Employees.Update(model);
+            return  _dBContext.SaveChanges();
+        }
+        public int delete(Employee model)
+        {
+            _dBContext.Employees.Remove(model);
+            return _dBContext.SaveChanges();
+        }
+
+      
+
+      
+    }
+}
