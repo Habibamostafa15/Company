1218458c42a6b8d2425457174180c47581bdb4b0
habiba <habiba.mostafa.567@gmail.com>
2025-04-04 09:22:21 +0200

    implement client side validation

--- Company.G02.PL/Controllers/EmployeeController.cs
+++ Company.G02.PL/Controllers/EmployeeController.cs
@@ -3,6 +3,9 @@ using Company.BLL.Reposotiry;
 using Company.DAL.Dtos;
 using Company.DAL.Entites;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Company.PL.Controllers
 {
@@ -65,31 +68,56 @@ namespace Company.PL.Controllers
         [HttpGet]
         public IActionResult Edit(int? id)
         {
-            //if (id is null) return BadRequest("InValid Id");
-            //var department = _departmentReposirtory.GetDepartment(id.Value);
-            //if (department is null) return NotFound(new { StatusCode = 404, Message = $"department with id {id} is not found" });
-            return Details(id,"Edit");
+            if (id is null) return BadRequest("InValid Id");
+            var employee = _EmployeeReposirtory.Get(id.Value);
+            if (employee is null) return NotFound(new { StatusCode = 404, Message = $"department with id {id} is not found" });
+            var EmployeeDTO = new CreateEmployeeDto
+            {
+               
+                Name = employee.Name,
+                Address = employee.Address,
+                Age = employee.Age,
+                CreateAt = employee.CreateAt,
+                HiringDate = employee.HiringDate,
+                IsActive = employee.IsActive,
+                IsDeleted = employee.IsDeleted,
+                Phone = employee.Phone,
+                Salary = employee.Salary
+            };
+            return View(EmployeeDTO);
 
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit([FromRoute] int id, Employee employee)
+        public IActionResult Edit([FromRoute] int id, CreateEmployeeDto model)
         {
             if (ModelState.IsValid)
             {
-                if (id == employee.Id)
-                {
-                    var count = _EmployeeReposirtory.Update(employee);
+                // if (id == employee.Id) return BadRequest();
+
+                var Employee = new Employee
+                {    Id=id,
+                    Name = model.Name,
+                    Address = model.Address,
+                    Age = model.Age,
+                    CreateAt = model.CreateAt,
+                    HiringDate = model.HiringDate,
+                    IsActive = model.IsActive,
+                    IsDeleted = model.IsDeleted,
+                    Phone = model.Phone,
+                    Salary = model.Salary
+                };
+                var count = _EmployeeReposirtory.Update(Employee);
                     if (count > 0)
                     {
                         return RedirectToAction(nameof(Index));
                     }
-                }
+                
 
             }
 
-            return View(employee);
+            return View(model);
         }
 
 
